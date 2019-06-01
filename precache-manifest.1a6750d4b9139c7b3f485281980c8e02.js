4dc3ba7f3db5daa776a78ffca8fc13eec3b0c27a
rama <purna.july10@gmail.com>
2019-06-01 13:17:43 +0530

    Updates

---
+++ precache-manifest.1a6750d4b9139c7b3f485281980c8e02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8fe9a79be35220c7d038338f61af28b",
+    "url": "/react-robofriends/index.html"
+  },
+  {
+    "revision": "8d8ad546c8bba8b27b11",
+    "url": "/react-robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "9603e789e566716b25de",
+    "url": "/react-robofriends/static/css/main.1506d579.chunk.css"
+  },
+  {
+    "revision": "8d8ad546c8bba8b27b11",
+    "url": "/react-robofriends/static/js/2.c05ccf0c.chunk.js"
+  },
+  {
+    "revision": "9603e789e566716b25de",
+    "url": "/react-robofriends/static/js/main.98bed9ff.chunk.js"
+  },
+  {
+    "revision": "238297068e2e665c321e",
+    "url": "/react-robofriends/static/js/runtime~main.41660cae.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
